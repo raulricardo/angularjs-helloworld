580c491fc4d147a3313ccc55f0e24808d880fc0a
Raúl Ricardo Amaya <raulricardo21+precise@gmail.com>
2013-07-06 00:35:26 -0500

    First use of model, I think I'm starting to know what I'm doing

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,9 @@
 function InvoiceCntl($scope) {
-    $scope.qty = 1;
-    $scope.cost = 19.95;
-    $scope.discount = false;
+    $scope.qty = 1
+    $scope.cost = 19.95
+    $scope.discount = false
+}
+
+function listCtrl($scope) {
+    $scope.features = ['Precio', 'Confiabilidad', 'Garantia']
 }
